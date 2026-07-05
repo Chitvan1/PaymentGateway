68141d83c3e47bf8c5f0c681d94604c1e843b419
Chitvan Gupta <chitvangupta01@gmail.com>
2026-07-05 23:00:26 +0530

    "Fixed Apikey and ResourceNotFoundException"

--- src/main/java/com/paymentSystem/razorpay/common/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/paymentSystem/razorpay/common/exceptions/ResourceNotFoundException.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 @Getter
 public class ResourceNotFoundException extends RuntimeException {
     private final String resourceName;
-    private final String identifier;
+    private final Object identifier;
 
-    public ResourceNotFoundException(String resourceName, String identifier) {
+    public ResourceNotFoundException(String resourceName, Object identifier) {
         super(resourceName + " not found: " + identifier);
         this.resourceName = resourceName;
         this.identifier = identifier;
--- src/main/java/com/paymentSystem/razorpay/merchant/entity/ApiKey.java
+++ src/main/java/com/paymentSystem/razorpay/merchant/entity/ApiKey.java
@@ -35,6 +35,7 @@ public class ApiKey {
     private Environment environment;
 
     @Column(nullable=false)
+    @Builder.Default
     private boolean enabled = true;
 
 
