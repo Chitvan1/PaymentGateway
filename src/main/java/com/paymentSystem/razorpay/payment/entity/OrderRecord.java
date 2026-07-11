652a5fc0753b3dab5de1ab69c2bd410b130cd136
Chitvan Gupta <chitvangupta01@gmail.com>
2026-07-11 18:47:09 +0530

    Refactor Money class to use Lombok annotations and add receipt field to OrderRecord

--- src/main/java/com/paymentSystem/razorpay/payment/entity/OrderRecord.java
+++ src/main/java/com/paymentSystem/razorpay/payment/entity/OrderRecord.java
@@ -30,10 +30,14 @@ public class OrderRecord {
     @Embedded
     private Money amount;
 
+    @Column(length = 100)
+    private String receipt;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private OrderStatus orderStatus = OrderStatus.CREATED;
 
+    @Builder.Default
     @Column(nullable = false)
     private Integer attempts = 0;
 
