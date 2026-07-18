d6c57d63f138d4cb8ac5e9061b73d4cf21c22939
Chitvan Gupta <chitvangupta01@gmail.com>
2026-07-18 10:55:33 +0530

    Enhance NetBankingAdapter: add logging and integrate PaymentProcessorRouter

--- src/main/java/com/paymentSystem/razorpay/payment/gateway/adapter/NetBankingAdapter.java
+++ src/main/java/com/paymentSystem/razorpay/payment/gateway/adapter/NetBankingAdapter.java
@@ -3,10 +3,22 @@ package com.paymentSystem.razorpay.payment.gateway.adapter;
 import com.paymentSystem.razorpay.payment.gateway.PaymentAdapter;
 import com.paymentSystem.razorpay.payment.gateway.dto.PaymentRequest;
 import com.paymentSystem.razorpay.payment.gateway.dto.PaymentResult;
+import com.paymentSystem.razorpay.payment.processor.PaymentProcessorRouter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
+@Component
+@Slf4j
+@RequiredArgsConstructor
 public class NetBankingAdapter implements PaymentAdapter {
+
+    private final PaymentProcessorRouter  paymentProcessorRouter;
+
     @Override
     public PaymentResult initiate(PaymentRequest request){
+        log.info("Initiate Payment with NetBankingAdapter, paymentId: {} " +request.paymentId());
+
         return null;
     }
 }
