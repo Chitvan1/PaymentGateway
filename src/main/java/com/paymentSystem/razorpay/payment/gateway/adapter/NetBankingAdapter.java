47230a44bb6446eced8bef978985fac842aba962
Chitvan Gupta <chitvangupta01@gmail.com>
2026-07-19 21:49:52 +0530

    Enhance NetBankingAdapter: implement payment processing logic and update PaymentResult interface

--- src/main/java/com/paymentSystem/razorpay/payment/gateway/adapter/NetBankingAdapter.java
+++ src/main/java/com/paymentSystem/razorpay/payment/gateway/adapter/NetBankingAdapter.java
@@ -1,9 +1,13 @@
 package com.paymentSystem.razorpay.payment.gateway.adapter;
 
+import com.paymentSystem.razorpay.common.enums.PaymentMethods;
 import com.paymentSystem.razorpay.payment.gateway.PaymentAdapter;
 import com.paymentSystem.razorpay.payment.gateway.dto.PaymentRequest;
 import com.paymentSystem.razorpay.payment.gateway.dto.PaymentResult;
+import com.paymentSystem.razorpay.payment.processor.PaymentProcessor;
 import com.paymentSystem.razorpay.payment.processor.PaymentProcessorRouter;
+import com.paymentSystem.razorpay.payment.processor.dto.PaymentProcessorRequest;
+import com.paymentSystem.razorpay.payment.processor.dto.PaymentProcessorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -13,12 +17,30 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class NetBankingAdapter implements PaymentAdapter {
 
-    private final PaymentProcessorRouter  paymentProcessorRouter;
+    private final PaymentProcessorRouter paymentProcessorRouter;
 
     @Override
     public PaymentResult initiate(PaymentRequest request){
         log.info("Initiate Payment with NetBankingAdapter, paymentId: {} " +request.paymentId());
+        PaymentProcessorRequest paymentProcessorRequest = PaymentProcessorRequest.nonCard(
+                request.paymentId(),
+                PaymentMethods.NETBANKING,
+                request.amount(),
+                request.methodDetails()
+        );
 
-        return null;
+        PaymentProcessorResponse paymentProcessorResponse = paymentProcessorRouter.
+                charge(paymentProcessorRequest);
+
+        return switch (paymentProcessorResponse) {
+            case PaymentProcessorResponse.Failure failure -> new PaymentResult.
+                    Failure(failure.errorCode(), failure.errorDescription());
+            case PaymentProcessorResponse.Pending pending -> new PaymentResult.
+                    Pending(pending.processorReference());
+            case PaymentProcessorResponse.Success success -> new PaymentResult.
+                    Success(success.processorReference());
+
+
+        };
     }
 }
--- src/main/java/com/paymentSystem/razorpay/payment/processor/PaymentProcessorRouter.java
+++ src/main/java/com/paymentSystem/razorpay/payment/processor/PaymentProcessorRouter.java
@@ -3,16 +3,18 @@ package com.paymentSystem.razorpay.payment.processor;
 import com.paymentSystem.razorpay.common.enums.PaymentMethods;
 import com.paymentSystem.razorpay.payment.processor.dto.PaymentProcessorRequest;
 import com.paymentSystem.razorpay.payment.processor.dto.PaymentProcessorResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class PaymentProcessorRouter {
 
     private Map<PaymentMethods, PaymentProcessor> paymentProcessorMap;
 
-    public PaymentProcessorResponse create(PaymentProcessorRequest request){
+    public PaymentProcessorResponse charge(PaymentProcessorRequest request){
             PaymentProcessor paymentProcessor = paymentProcessorMap.get(request.methods());
             if(paymentProcessor == null){
                 throw new IllegalArgumentException("No Payment processor registered for method: "+request.methods());
