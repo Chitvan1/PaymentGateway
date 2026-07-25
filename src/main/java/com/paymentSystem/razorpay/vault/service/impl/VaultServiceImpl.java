9927fe7b02f665c82486603f63a17331a5a719c2
Chitvan Gupta <chitvangupta01@gmail.com>
2026-07-25 15:16:38 +0530

    Enhance vault functionality: implement card tokenization and encryption for sensitive data

--- src/main/java/com/paymentSystem/razorpay/vault/service/impl/VaultServiceImpl.java
+++ src/main/java/com/paymentSystem/razorpay/vault/service/impl/VaultServiceImpl.java
@@ -1,8 +1,12 @@
 package com.paymentSystem.razorpay.vault.service.impl;
 
 import com.paymentSystem.razorpay.common.enums.CardBrand;
+import com.paymentSystem.razorpay.common.util.RandomizerUtil;
+import com.paymentSystem.razorpay.vault.config.VaultEncryptionConfig;
 import com.paymentSystem.razorpay.vault.dto.request.TokenizeRequest;
 import com.paymentSystem.razorpay.vault.dto.response.TokenizeResponse;
+import com.paymentSystem.razorpay.vault.entity.CardToken;
+import com.paymentSystem.razorpay.vault.entity.VaultCard;
 import com.paymentSystem.razorpay.vault.repository.CardTokenRepository;
 import com.paymentSystem.razorpay.vault.repository.VaultCardRepository;
 import com.paymentSystem.razorpay.vault.service.VaultService;
@@ -10,8 +14,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.encrypt.BytesEncryptor;
 import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -25,8 +31,10 @@ public class VaultServiceImpl implements VaultService {
 
     private final CardTokenRepository cardTokenRepository;
     private final VaultCardRepository vaultCardRepository;
+    private final BytesEncryptor dekEncryptor;
 
     @Override
+    @Transactional
     public TokenizeResponse tokenize(TokenizeRequest request, UUID merchantId) {
         String lastFour = request.pan().substring(request.pan().length() - 4);
         String bin = request.pan().substring(0, 6);
@@ -34,8 +42,29 @@ public class VaultServiceImpl implements VaultService {
 
         byte[] dek = KeyGenerators.secureRandom(32).generateKey();
         byte[] encryptedPan = panEncryptor(dek).encrypt(request.pan().getBytes(StandardCharsets.UTF_8));
-        return null;
+        byte[] encryptedDek = dekEncryptor.encrypt(dek);
+        VaultCard vaultCard = VaultCard.builder()
+                .brand(cardBrand)
+                .expiryYear(request.expiryYear().toString())
+                .expiryMonth(request.expiryMonth().toString())
+                .bin(bin)
+                .lastFour(lastFour)
+                .encryptedDek(encryptedDek)
+                .encryptedPan(encryptedPan)
+                .cardHolderName(request.cardHolderName())
+                .build();
 
+        vaultCard = vaultCardRepository.save(vaultCard);
+
+        String token = "tok_" + RandomizerUtil.randomBase64(32);
+        cardTokenRepository.save(CardToken.builder()
+                        .vaultCard(vaultCard)
+                        .token(token)
+                        .customer(request.customerId())
+                        .merchant(merchantId)
+                        .build());
+
+        return new TokenizeResponse(token, lastFour, cardBrand, request.expiryMonth(), request.expiryYear());
     }
 
     private CardBrand detectBrand(String pan) {
