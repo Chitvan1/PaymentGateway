b5287ef49b834bb435dc0cd4e4eaa2cf995f872e
Chitvan Gupta <chitvangupta01@gmail.com>
2026-08-01 23:12:34 +0530

    Enhance security configuration: integrate JwtAuthenticationFilter into security chain for improved authentication

--- src/main/java/com/paymentSystem/razorpay/merchant/security/WebSecurityConfig.java
+++ src/main/java/com/paymentSystem/razorpay/merchant/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.paymentSystem.razorpay.merchant.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,13 +11,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
     private static final String[] JWT_ROUTES = {"/v1/auth/**", "/v1/merchants/**", "/v1/admin/**", "/actuator/**"};
     private static final String[] API_KEY_ROUTES = {"/v1/orders/**", "/v1/payments/**", "/v1/vault/**"};
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain jwtChain(HttpSecurity http) {
@@ -28,7 +33,7 @@ public class WebSecurityConfig {
                     .authorizeHttpRequests(auth->auth.requestMatchers("/v1/auth/login", "/v1/auth/signup").permitAll()
                             .anyRequest().authenticated()
                     )
-                    .formLogin(form->form.disable())
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                     .build();
     }
 
